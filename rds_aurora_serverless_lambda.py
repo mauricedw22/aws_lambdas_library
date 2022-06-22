66d66d836a805f205dfd35d4670cfb2d94058c75
Maurice WALKER <mauricedw22@gmail.com>
2022-06-22 08:08:50 -0400

    aurora lambda

--- rds_aurora_serverless_lambda.py
+++ rds_aurora_serverless_lambda.py
@@ -5,7 +5,7 @@ rds_client = boto3.client('rds-data')
 
 db_name = 'auroraserverless1'
 db_cluster_arn = 'arn:aws:rds:us-east-1:330202998142:cluster:aurora-serverless-1'
-db_credentials_secrets_arn = 'arn:aws:secretsmanager:us-east-1:330202998142:secret:rds-db-credentials/cluster-TJQICM2URUN4CU3L75EFRP4YRA/admin-GPtWLF'
+db_credentials_secrets_arn = '<SECRETS_ARN>'
 
 def lambda_handler(event, context):
     response = execute_statement('SELECT * FROM auroraserverless1.Customers')
